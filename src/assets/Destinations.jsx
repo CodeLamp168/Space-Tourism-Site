c97e3b898fbd89bdbbda118b453c69b108dc50e7
Caleb D. Sim <dylansim168@gmail.com>
2023-08-08 15:49:18 +0800

    Interactive template for destination webpage

--- src/assets/Destinations.jsx
+++ src/assets/Destinations.jsx
@@ -10,16 +10,13 @@ export default function Destinations(){
         return(
         <>
          <div className="main-planet-display-component">
-            <div className="planet-display-header">
-                <p><span>01</span>PICK YOUR DESTINATION</p>
-            </div>
             <div className="planet-display">
                 <img className="planet" src="/destination/image-moon.png" alt="404" />
             </div>
          </div>
          
          <div className="main-planet-interactive-component">
-             <nav>
+             <nav className="planet-interactive-nav-row">
                 <button>MOON</button>
                 <button>MARS</button>
                 <button>EUROPA</button>
@@ -28,6 +25,16 @@ export default function Destinations(){
              <div>
                 <h1>MOON</h1>
                 <p>See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.</p>
+                <div className="display-distance-travel-time">
+                    <div>
+                        <p>AVG. DISTANCE</p>
+                        <h2>384,400</h2>
+                    </div>
+                    <div>
+                        <p>AVG. DISTANCE</p>
+                        <h2>3 DAYS</h2>
+                    </div>
+                </div>
              </div>
 
          </div>
