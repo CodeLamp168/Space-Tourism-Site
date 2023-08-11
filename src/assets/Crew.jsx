cd765feff82f5afc59367241b5dbb59525fe7708
Caleb D. Sim <dylansim168@gmail.com>
2023-08-11 10:03:57 +0800

    useState functionality for Crew page

--- src/assets/Crew.jsx
+++ src/assets/Crew.jsx
@@ -1,52 +1,59 @@
+import React, { useState } from 'react';
 import CorrespondingBackgroundImage from "./ResponsiveBackgrounds";
 import Label from "./PageLabel";
-export default function CrewMembers(){
-    const destinationDesktop = '/crew/background-crew-desktop.jpg'
-    const destinationTablet = '/crew/background-crew-tablet.jpg'
-    const destinationMobile = '/crew/background-crew-mobile.jpg'
+import data from './data.json';
 
-    function CrewDetails() {
-        return(
-            <div className="crew-detail-container">
-                <div className="crew-text-info">
-                    <h3 className="crew-role">COMMANDER</h3>
-                    <h2 className="crew-name">DOUGLAS HURLEY</h2>
-                    <p className="crew-info">Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2</p>
-                </div>
-                <div className="crew-btn-list">
-                    <button className="crew-display-btn"></button>
-                    <button className="crew-display-btn"></button>
-                    <button className="crew-display-btn"></button>
-                    <button className="crew-display-btn"></button>
-                </div>
-            </div>
-        )
-    }
+export default function CrewMembers() {
+  const destinationDesktop = '/crew/background-crew-desktop.jpg';
+  const destinationTablet = '/crew/background-crew-tablet.jpg';
+  const destinationMobile = '/crew/background-crew-mobile.jpg';
 
-    function CrewImage(){
-
-        return (
-        <div className="crew-image-container">
-            <img src="/crew/image-douglas-hurley.png" alt="404" />
-        </div>
-        )
-    }
-
-
-
-
+  function CrewDisplayComponent({ crewMember, selectedCrewIndex, onCrewButtonClick }) {
     return (
-        <>
-       
-        <CorrespondingBackgroundImage
-         desktopImageUrl={destinationDesktop}
-         tabletImageUrl={destinationTablet}
-         mobileImageUrl={destinationMobile}/>
-        <main className="main-content-container crew-content-container">
-          <Label labelNumber={'02'} labelHeader={'MEET YOUR CREW'} labelWebClass={'crew-label'} />
-          <CrewDetails/>
-          <CrewImage/>
-        </main>
+      <>
+        <div className="crew-detail-container">
+          <div className="crew-text-info">
+            <h3 className="crew-role">{crewMember.role}</h3>
+            <h2 className="crew-name">{crewMember.name}</h2>
+            <p className="crew-info">{crewMember.bio}</p>
+          </div>
+          <div className="crew-btn-list">
+            {data.crew.map((member, index) => (
+              <button
+                key={index}
+                className={`crew-display-btn ${selectedCrewIndex === index ? 'active-crew-display-btn' : ''}`}
+                onClick={() => onCrewButtonClick(index)}
+              >
+              </button>
+            ))}
+          </div>
+        </div>
+  
+        <div className="crew-image-container">
+          <img src={crewMember.images.png} alt="404" />
+        </div>
       </>
-    )
+    );
+  }
+
+  const [selectedCrew, setSelectedCrew] = useState(0);
+  const selectedCrewMember = data.crew[selectedCrew];
+
+  return (
+    <>
+      <CorrespondingBackgroundImage
+        desktopImageUrl={destinationDesktop}
+        tabletImageUrl={destinationTablet}
+        mobileImageUrl={destinationMobile}
+      />
+      <main className="main-content-container crew-content-container">
+        <Label labelNumber={'02'} labelHeader={'MEET YOUR CREW'} labelWebClass={'crew-label'} />
+        <CrewDisplayComponent
+          crewMember={selectedCrewMember}
+          selectedCrewIndex={selectedCrew}
+          onCrewButtonClick={setSelectedCrew}
+        />
+      </main>
+    </>
+  );
 }
