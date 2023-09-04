ccd89c797045f89fa4ed3160bbd2cd202b6eeb9d
Caleb D. Sim <dylansim168@gmail.com>
2023-09-04 10:23:58 +0800

    Router configuration. issues with CSS and refresh

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import MainNavComponent from './assets/MainNav';
 import Home from './assets/Home';
-import './App.css'
+import './App.css';
 import Destinations from './assets/Destinations';
 import CrewMembers from './assets/Crew';
 import TechPage from './assets/Technology';
@@ -9,27 +10,19 @@ import TechPage from './assets/Technology';
 function App() {
   const [activePage, setActivePage] = useState('home');
 
-  const renderPage = () => {
-    switch (activePage) {
-      case 'home':
-        return <Home />;
-      case 'destinations':
-        return <Destinations />;
-      case 'crew':
-        return <CrewMembers />;
-      case 'tech': 
-        return <TechPage/>;
-      default:
-        return <Home />;
-    }
-  };
-
   return (
-    <div className="App">
-       <MainNavComponent activePage={activePage} setActivePage={setActivePage}/>
-       {renderPage()}
-    </div>
-  )
+    <Router>
+      <div className="App">
+        <MainNavComponent activePage={activePage} setActivePage={setActivePage} />
+        <Routes>
+           <Route exact path="/Space-Tourism-Site" element={<Home/>} component={Home}/>
+           <Route path="/Destinations" element={<Destinations/>}component={Destinations}/>
+           <Route path="/Crew" element={<CrewMembers/>} component={CrewMembers}/>
+           <Route path="/Technology" element={<TechPage/>} component={TechPage}/>
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
